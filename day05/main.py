13aac5b6a9a9c0de70d82c8acbbadd489f63c822
Maxime Lucas <maxime@mlcs.me>
2021-12-05 20:20:47 +0100

    Reorganize Day 5

--- day05/main.py
+++ day05/main.py
@@ -10,44 +10,23 @@ def extract_bounds(data):
     return numpy.array([format_bound(line) for line in data], dtype=int)
 
 
-def complete_diagram_without_diagonal(size, coordinates):
+def complete_diagram(size, coordinates, with_diagonal):
     diag = numpy.full([size, size], 0, dtype=int)
 
     for line in coordinates:
-        x1, y1 = line[0]
-        x2, y2 = line[1]
+        [x1, y1], [x2, y2] = line
+        min_x, max_x = min(x1, x2), max(x1, x2) + 1
+        min_y, max_y = min(y1, y2), max(y1, y2) + 1
 
         if x1 != x2 and y1 != y2:
-            continue
+            if not with_diagonal:
+                continue
 
-        start_x = min(x1, x2)
-        end_x = max(x1, x2)
-
-        start_y = min(y1, y2)
-        end_y = max(y1, y2)
-
-        for i in range(start_x, end_x + 1):
-            for j in range(start_y, end_y + 1):
-                diag[j][i] += 1
-
-    return diag
-
-
-def complete_diagram(size, coordinates):
-    diag = numpy.full([size, size], 0, dtype=int)
-
-    for line in coordinates:
-        x1, y1 = line[0]
-        x2, y2 = line[1]
-        diff_x = abs(x1 - x2)
-        diff_y = abs(y1 - y2)
-
-        if x1 != x2 and y1 != y2:
-            x_list = list(range(min(x1, x2), max(x1, x2) + 1))
+            x_list = list(range(min_x, max_x))
             if x1 > x2:
                 x_list.reverse()
 
-            y_list = list(range(min(y1, y2), max(y1, y2) + 1))
+            y_list = list(range(min_y, max_y))
             if y1 > y2:
                 y_list.reverse()
 
@@ -56,14 +35,8 @@ def complete_diagram(size, coordinates):
             for coord in coords:
                 diag[coord[1]][coord[0]] += 1
         else:
-            start_x = min(x1, x2)
-            end_x = max(x1, x2)
-
-            start_y = min(y1, y2)
-            end_y = max(y1, y2)
-
-            for i in range(start_x, end_x + 1):
-                for j in range(start_y, end_y + 1):
+            for i in range(min_x, max_x):
+                for j in range(min_y, max_y):
                     diag[j][i] += 1
 
     return diag
@@ -73,25 +46,19 @@ def count_overlapping(diagram):
     return numpy.count_nonzero(diagram > 1)
 
 
-def get_answer_part1(size, data):
+def get_answer(size, data, with_diagonal=False):
     bounds = extract_bounds(data)
-    diagram = complete_diagram_without_diagonal(size, bounds)
-    return count_overlapping(diagram)
-
-
-def get_answer_part2(size, data):
-    bounds = extract_bounds(data)
-    diagram = complete_diagram(size, bounds)
+    diagram = complete_diagram(size, bounds, with_diagonal)
     return count_overlapping(diagram)
 
 
 example_data, input_data = utils.get_data("05")
 
-example_answer1 = get_answer_part1(10, example_data)
-input_answer1 = get_answer_part1(1000, input_data)
+example_answer1 = get_answer(10, example_data)
+input_answer1 = get_answer(1000, input_data)
 
-example_answer2 = get_answer_part2(10, example_data)
-input_answer2 = get_answer_part2(1000, input_data)
+example_answer2 = get_answer(10, example_data, with_diagonal=True)
+input_answer2 = get_answer(1000, input_data, with_diagonal=True)
 
 print("## Part 1 ##")
 print(f"Answer: {example_answer1}")
