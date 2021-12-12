bed2fc2e3b00d187e85066bb4a005078cddb013b
Maxime Lucas <maxime@mlcs.me>
2021-12-12 16:34:04 +0100

    Add Day 9 Part 1

---
+++ day09/main.py
@@ -0,0 +1,46 @@
+from utils import utils
+from numpy import array, count_nonzero
+
+
+def clamp(num, min_value, max_value):
+    return max(min(num, max_value), min_value)
+
+
+def calc_risk_level_sum(data):
+    columns = len(data[0])
+    lines = len(data[:, 0])
+    risk_sum = 0
+
+    for line in range(lines):
+        for column in range(columns):
+            element = data[line][column]
+
+            column_prev = clamp(column - 1, 0, columns)
+            column_next = clamp(column + 1, 0, columns)
+
+            line_prev = clamp(line - 1, 0, lines)
+            line_next = clamp(line + 1, 0, lines)
+
+            horizontal = data[line, column_prev:column_next + 1]
+            vertical = data[line_prev:line_next + 1, column]
+
+            is_unique_horizontal = count_nonzero(horizontal == element) == 1
+            is_unique_vertical = count_nonzero(vertical == element) == 1
+            is_unique = is_unique_vertical and is_unique_horizontal
+
+            if is_unique and element == min(horizontal) and element == min(vertical):
+                risk_sum += (element + 1)
+
+    return risk_sum
+
+
+example_data, input_data = utils.get_data("09")
+example_data = array([list(line) for line in example_data]).astype(int)
+input_data = array([list(line) for line in input_data]).astype(int)
+
+example_answer = calc_risk_level_sum(example_data)
+input_answer = calc_risk_level_sum(input_data)
+
+print("## Part 1 ##")
+print(f"Answer: {example_answer}")
+print(f"Answer: {input_answer}\n")
